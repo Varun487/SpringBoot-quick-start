e32a85772e4c281e077089f8e9a7a6ac4268a2af
Varun487 <seshuvarun@gmail.com>
2021-01-15 23:51:22 +0530

    Inital project

---
+++ springbootquickstart/src/test/java/com/varun/springbootquickstart/SpringbootquickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.springbootquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootquickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
